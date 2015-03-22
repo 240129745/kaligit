57852d96bf1758390ebf4a8d25e05140ea7ac962
240129745 <240129745@qq.com>
2015-03-22 12:09:41 +0800

    ok

--- getinfo.c
+++ getinfo.c
@@ -6,8 +6,9 @@ extern struct passwd *pwd;
 void getinfo()
 {
 	pwd=getpwuid(getuid());
+	assert(NULL!=pwd);
+
 	printf("the user name is:%s\n",pwd->pw_name);
 	printf("uid is %d\n",getuid());
-	assert(0);
 }
 
